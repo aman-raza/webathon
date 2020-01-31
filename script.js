699b00972794e8b176d7c46e66a7296272981089
AMAN RAZA <53443872+amanraza1234@users.noreply.github.com>
2020-01-31 16:43:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var my Text = document.getElementaryById("new").value
+
+function show() {
+    let lbl = document.getElementById("");
+    let = document.getElementById("new").value;
+
+    lbl.innerText =
+}
